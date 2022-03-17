20310d18794e8583ec3dec31ede18e43cb2cb56b
ABHISHEK KOGNOLE <akognole@outerbanks.umaryland.edu>
2022-03-17 14:59:35 -0400

    define cutoff

--- 02_process_mme.py
+++ 02_process_mme.py
@@ -2,6 +2,9 @@ import numpy as np
 from sys import argv
 import os
 
+# cutoff for the maximum energy from minima to include
+cutoff = 15.0 # kcal/mol
+
 resid, a1, a2, a3, a4 = str(argv[1]), str(argv[2]), str(argv[3]), str(argv[4]), str(argv[5])
 jobname="%s_dihe_%s_%s_%s_%s"%(resid,a1,a2,a3,a4)
 
@@ -9,10 +12,9 @@ a = np.loadtxt('output/%s/mm_%s.txt'%(jobname,jobname))
 
 emin = min(a[:,1])
 mme = []
-iters = []
 
 for i,e in enumerate(a[:,1]):
-    if e < emin+15:
+    if e < emin+cutoff:
         mme.append((a[i,0],a[i,1],a[i,2]))
 
 mme = np.array(mme)
@@ -29,11 +31,11 @@ for i,j in enumerate(a[:,0]):
 
 f = open('output/%s/%s.mme'%(jobname,jobname), 'w')
 a = a[a[:, 0].argsort()]
+iters = []
 for i,j in enumerate(a[:,0]):
     f.write("%.2f %.3f %d\n"%(a[i,0],a[i,1],i+1001))
     iters.append((a[i,0],a[i,2],i+1001))
 f.close()        
-
 iters = np.array(iters)
 
 for i,j in enumerate(iters[:,1]):
